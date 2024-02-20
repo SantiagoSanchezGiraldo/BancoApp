186daa12abbc36a9241a880665e66947c7cccc2f
Santiago Sánchez <sanchez.gsanti@gmail.com>
2024-02-19 22:21:02 -0500

    Nueva Seccion de la App Dashboard

---
+++ app/src/main/java/com/logapp/InicioBanc.java
@@ -0,0 +1,14 @@
+package com.logapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioBanc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_banc);
+    }
+}
--- app/src/main/java/com/logapp/MainActivity.java
+++ app/src/main/java/com/logapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.logapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,11 +25,11 @@ public class MainActivity extends AppCompatActivity {
         claveInput = findViewById(R.id.clave_input);
         loginBtn = findViewById(R.id.login_btn);
 
-        login_btn.setOnClickListener(new View.OnClickListener(){
+        loginBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent login_btn = new Intent(MainActivity.this, InicioBanc.class);
-                startActivity(login_btn);
+                Intent loginBtn = new Intent(MainActivity.this, InicioBanc.class);
+                startActivity(loginBtn);
             }
         });
         
